c033bcb8ad1c2b1aff5cbe87f763e42430666bb8
Disonlono <diegogomessilva900@gmail.com>
2024-11-07 00:24:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'O que é um "phrasal verb"?',
+    'É uma combinação de verbo e preposição ou advérbio que altera o sentido do verbo original.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um espaço na memória que armazena dados temporários.'
+)
+
+criaCartao(
+    'Jogos',
+    'O que significa "FPS" em jogos?',
+    '"FPS" pode significar "First-Person Shooter" (jogo de tiro em primeira pessoa) ou "Frames Per Second" (quadros por segundo).'
+)
